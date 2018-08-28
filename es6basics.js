adb653051a4484f9b4e586f28ba77779e9a67839
Shaowen Yao <dsydesign@gmail.com>
2018-08-28 15:54:29 -0700

    es6 hoist

--- es6basics.js
+++ es6basics.js
@@ -28,8 +28,37 @@ function blockscopefunc() {
 	}
 }
 
-//ES6 BETTER FOR LOOP + ARROW FUNC
+//ES6 BETTER FOR LOOP 
 let forarray = [1,2,3];
 for (let value of forarray) { console.log(value); }
-//ES6 FOREACHLOOP
+//ES6 FOREACHLOOP + ARROW FUNC
+forarray.forEach( function(item) { console.log(item)});
 forarray.forEach( anything_justhastomatch => console.log(anything_justhastomatch));
+
+//VALUE WITH let vs const
+console.log(thisisnotdefinedyet);
+let thisisnotdefinedyet="this cannot be accessed, put above";
+
+console.log(thisisnotdefinedyet_NOTLET);
+let thisisnotdefinedyet_NOTLET="this still returns undefined";
+
+//ES6 IS BLOCK SCOPED
+if (true) { let y = "this is not in scope of the console call"; }
+console.log(y + "y refers to nothing, unless there is prev block or global/func var");
+
+//ES6 ANON FUNC
+(function() { console.log("self-executes anon func") })();
+
+//FUNCTIONAL HOSTING
+is_hoisted();
+is_hoisted function () { };
+is_NOT_hoisted();
+var is_NOT_hoisted = function ( "declarations are hoisted, expressions are not")
+
+//VAR HOISTING var only
+console.log(x + "can use but value not transferred!"); // true
+var x = "value is declared";
+
+y="DO NOT HOIST LETS OR CONSTS";
+var y;
+console.log(y + "this returns value")
